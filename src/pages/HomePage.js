f12e41d85b729c439a190ed971a5fa84bcf08cc0
KateGordin <kate.gordin@gmail.com>
2022-04-18 15:07:32 +0200

    LP-7: Set background image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,6 @@
 import React from "react";
+import "./HomePage.scss";
 
 export default function HomePage() {
-  return (
-    <div>
-      HomePage
-      <h4>Here will be main big and nice image</h4>
-    </div>
-  );
+  return <div className="homePage"></div>;
 }
