21ec8893238bef1544b11d813ce6fa705f4fa415
KateGordin <kate.gordin@gmail.com>
2022-04-26 17:16:30 +0200

    LP-38: show total price in cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,26 +4,28 @@ import { useState, useEffect } from "react";
 import "./Cart.scss";
 import DatePicker from "react-datepicker";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchOrder } from "../store/orders/actions";
 import { selectCart } from "../store/orders/selectors";
 import { deleteOrderItem } from "../store/orders/actions";
 import { submitOrder } from "../store/orders/actions";
 import { useNavigate } from "react-router-dom";
 import { NotificationManager } from "react-notifications";
+import { selectToken } from "../store/user/selectors";
 
 export default function Cart() {
+  const token = useSelector(selectToken);
+
   const [myEventName, setEventName] = useState(" ");
   const navigate = useNavigate();
 
   //my text in email to actors
   const [textInEmail, setTextInEmail] = useState(" ");
 
+  //sum of all actors
+  const [sum, setSum] = useState(0);
+
   const dispatch = useDispatch();
   const cart = useSelector(selectCart);
-  const fetchMyOrderItems = async () => {
-    dispatch(fetchOrder);
-  };
-
+  console.log("cart", cart);
   //delete one order item from cart
   const deleteMyOrderItem = async (id) => {
     dispatch(await deleteOrderItem(id));
@@ -37,71 +39,88 @@ export default function Cart() {
     navigate("/");
     NotificationManager.success(
       "We will answer you soon",
-      "Order successfully submitted"
+      "🥳 Order successfully submitted "
     );
   };
 
   useEffect(() => {
-    fetchMyOrderItems();
-  }, []);
+    if (token === null) {
+      navigate("/");
+    }
+  }, [token, navigate]);
+
+  useEffect(() => {
+    if (cart) {
+      let sumInCart = 0;
+      cart.orderItems.forEach(
+        (orderItem) => (sumInCart += orderItem.actor.price)
+      );
+      setSum(sumInCart);
+    }
+  }, [cart]);
 
   // const [startDate, setStartDate] = useState(new Date());
 
   return (
-    <div className="container">
-      <h4>Birthday cart</h4>
+    <>
+      <div className="container">
+        <h4>Birthday cart</h4>
 
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Actors</th>
-            <th>Duration</th>
-            <th>Price</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Actors</th>
+              <th>Duration</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {cart &&
-            cart.orderItems.map((orderItem, index) => (
-              <tr key={orderItem.id}>
-                <td>{index + 1}</td>
-                <td>{orderItem.actor.name}</td>
-                <td>1 hour</td>
-                <td>{orderItem.actor.description} €</td>
-                <td>
-                  <button onClick={() => deleteMyOrderItem(orderItem.id)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
+          <tbody>
+            {cart &&
+              cart.orderItems.map((orderItem, index) => (
+                <tr key={orderItem.id}>
+                  <td>{index + 1}</td>
+                  <td>{orderItem.actor.name}</td>
+                  <td>1 hour</td>
+                  <td>{orderItem.actor.price} €</td>
+                  <td>
+                    <button onClick={() => deleteMyOrderItem(orderItem.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+          <div style={{ fontWeight: "bold", fontSize: "150%" }}>
+            Total price: {sum} €
+          </div>
+        </Table>
 
-      {/* <DatePicker
+        {/* <DatePicker
         selected={startDate}
         onChange={(date) => setStartDate(date)}
       /> */}
 
-      <div>
-        <label>Event Name: </label>
-        <input
-          type="text"
-          value={myEventName}
-          onInput={(e) => setEventName(e.target.value)}
-        />
-        <br></br>
-        <label>Add your preferences: </label>
-        <input
-          type="text"
-          value={textInEmail}
-          onInput={(e) => setTextInEmail(e.target.value)}
-        />
-      </div>
+        <div>
+          <label>Event Name: </label>
+          <input
+            type="text"
+            value={myEventName}
+            onInput={(e) => setEventName(e.target.value)}
+          />
+          <br></br>
+          <label>Add your preferences: </label>
+          <input
+            type="text"
+            value={textInEmail}
+            onInput={(e) => setTextInEmail(e.target.value)}
+          />
+        </div>
 
-      <button onClick={submitOrderAndClearState}>Order</button>
-    </div>
+        <button onClick={submitOrderAndClearState}>Order</button>
+      </div>
+    </>
   );
 }
