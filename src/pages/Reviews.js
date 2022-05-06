a19410c93635dfe0b7e05e5fbda8398943e0856e
KateGordin <kate.gordin@gmail.com>
2022-05-06 11:11:52 +0200

    Reviews harcoded

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./Reviews.scss";
 
 export default function Reviews() {
-  return <div className="reviews"></div>;
+  return (
+    <div className="reviews">
+      <h3>What our clients say about LUCKY:</h3>
+      <h4>LUCKY is the best agency. Thanks for the party.</h4>
+      <h4> We had B-day with Spiderman. We are happy. Thank you. </h4>
+      <h4>Thank yoy LUCKY for Santa-party. Highly recommended.</h4>
+    </div>
+  );
 }
